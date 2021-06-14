0a3cde6e41b27ff45df41d65f941c7f43fa7cd69
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2021-06-14 22:42:43 +0530

    Create Next Greater Element III.py

---
+++ Next Greater Element III.py
@@ -0,0 +1,30 @@
+#https://leetcode.com/problems/next-greater-element-iii
+
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        s = str(n)
+        s_list = [c for c in s]
+        l = len(s)
+        
+        if l == 1:
+            return -1
+        
+        i = l - 2
+        
+        while i >= 0 and s_list[i] >= s_list[i + 1] :
+            i -= 1
+        
+        if i < 0:
+            return -1
+        
+        j = l - 1
+        while s_list[j] <= s_list[i]:
+            j -= 1
+        
+        s_list[j], s_list[i] = s_list[i], s_list[j]
+        
+        s_list[i+1:] = reversed(s_list[i+1:])
+        
+        ans = ''.join(s_list)
+        return int(ans) if int(ans) < 2**31 else -1
+        
