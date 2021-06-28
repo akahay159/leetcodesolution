cd7d0da98d9234f1910defd5ebe533c266ac19fd
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2021-06-28 20:53:32 +0530

    Create Intersection_of_List.kt

---
+++ Intersection_of_List.kt
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+class Solution {
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        var a = headA
+        var b = headB
+        while(a != b){
+            a = if(a == null) headB else a.next
+            b = if(b == null) headA else b.next
+        }
+        return a
+        
+    }
+}
