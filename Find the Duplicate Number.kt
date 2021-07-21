8a6204cbd6818f1778364c60f61b68330bc954bd
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2021-07-21 21:53:00 +0530

    Create Find the Duplicate Number.kt
    There is only one repeated number in nums, return this repeated number. kotlin Solution

---
+++ Find the Duplicate Number.kt
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/find-the-duplicate-number
+
+class Solution {
+    fun findDuplicate(nums: IntArray): Int {
+        
+        // Solution 2. Time -> O(N) and Space -> O(1)
+        for(item in nums){
+            if((nums[Math.abs(item) - 1]) < 0){
+                return Math.abs(item)
+            }
+            nums[Math.abs(item)-1] *= -1
+        }
+        return -1
+        
+        
+        /*
+        // Solution 1. Time -> O(N) and Space -> O(N)
+        val helper = mutableSetOf<Int>()
+        for(item in nums){
+            if(item in helper){
+                return item
+            }
+            helper.add(item)
+        }
+        return -1
+        */
+    }
+}
