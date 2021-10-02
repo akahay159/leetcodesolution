d7d753ea0721aae0821b59110a2451992c3a75ba
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2021-10-02 21:21:03 +0530

    Update robberHouse.kt

--- robberHouse.kt
+++ robberHouse.kt
@@ -1,24 +1,46 @@
-// https://leetcode.com/problems/house-robber
-// brute force approach will give TLE
-// use dp concept for not getting TLE
 class Solution {
-    fun solution(nums: IntArray, start: Int, end:Int): Int{
-        if(start > end){
+    
+    fun rob(nums: IntArray): Int {
+        var n = nums.size
+        var max_value = 0
+        if(n == 0){
             return 0
         }
-        if(start == end){
-            return nums[start]
+        if(n == 1){
+            return nums[0]
         }
-        if(start + 1 == end){
-            return Math.max(nums[start], nums[end])
+        var value1 = nums[0]
+        var value2 = Math.max(nums[0], nums[1])
+        
+        if(n == 2){
+            return value2
         }
+        for(i in 2 until n){
+            max_value = Math.max(nums[i] + value1, value2)
+            value1 = value2
+            value2 = max_value
+        }
+        return max_value
         
-        return Math.max(nums[start] + solution(nums, start+2, end), solution(nums, start+1, end))
-    }
-    fun rob(nums: IntArray): Int {
-        var start = 0
-        var end = nums.size - 1
-        
-        return solution(nums, start, end)
     }
 }
+//     fun solution(nums: IntArray, start: Int, end:Int): Int{
+//         if(start > end){
+//             return 0
+//         }
+//         if(start == end){
+//             return nums[start]
+//         }
+//         if(start + 1 == end){
+//             return Math.max(nums[start], nums[end])
+//         }
+        
+//         return Math.max(nums[start] + solution(nums, start+2, end), solution(nums, start+1, end))
+//     }
+//     fun rob(nums: IntArray): Int {
+//         var start = 0
+//         var end = nums.size - 1
+        
+//         return solution(nums, start, end)
+//     }
+//}
