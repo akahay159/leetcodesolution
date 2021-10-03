55f995838a73a005f199260e4eda99159b058a70
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2021-10-03 19:06:29 +0530

    Update pivotelement.py

--- pivotelement.py
+++ pivotelement.py
@@ -1,16 +1,27 @@
 # https://leetcode.com/problems/find-pivot-index/
 
-
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
-        leftsum = 0
-        rightsum = sum(nums)
+        ls = 0
+        rs = 0
         for i in range(len(nums)):
-            if i > 0:
-                leftsum += nums[i - 1]
-            rightsum -= nums[i]
-            if rightsum == leftsum:
+            ls = sum(nums[:i])
+            rs = sum(nums[i+1:])
+            if rs == ls:
                 return i
+            ls = 0
+            rs = 0
         return -1
+    
+        # leftsum = 0
+        # rightsum = sum(nums)
+        # for i in range(len(nums)):
+        #     if i > 0:
+        #         leftsum += nums[i - 1]
+        #     rightsum -= nums[i]
+        #     if rightsum == leftsum:
+        #         return i
+        # return -1
+        
         
